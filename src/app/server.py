104be9d901a1a59b7322fcd6688919dd44556521
majid mohamadi <m.shutdown57@gmail.com>
2018-07-27 10:06:15 -0400

    init project

---
+++ src/app/server.py
@@ -0,0 +1,18 @@
+from app import app
+import cherrypy
+
+
+if __name__ == '__main__':
+    cherrypy.tree.graft(app, "/")
+    cherrypy.server.unsubscribe()
+
+    server = cherrypy._cpserver.Server()
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 5000
+    server.thread_pool = 30
+
+    server.subscribe()
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
