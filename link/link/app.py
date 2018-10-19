52c0920e011074f1450e24e9bf91ef79edffc1e8
majid mohamadi <m.shutdown57@gmail.com>
2018-10-19 05:19:13 +0330

    fixed root route 404 error

--- link/link/app.py
+++ link/link/app.py
@@ -1,12 +1,5 @@
-from flask import render_template
-
 from server import app
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-
-#  if __name__ == '__main__':
-    #  app.run(debug=False)
+if app:
+    print("*" * 8, "app initialized", "*" * 8)
--- link/link/factory.py
+++ link/link/factory.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from config import configs
 
@@ -6,5 +6,9 @@ from config import configs
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(configs[config_name])
+
+    @app.route('/', methods=['GET'])
+    def index():
+        return render_template('index.html')
 
     return app
