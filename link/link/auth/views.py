184d305d17c2cfb7628bd372e8654c1176726edb
majid mohamadi <m.shutdown57@gmail.com>
2018-11-26 11:39:06 +0330

    fixed
    register_login route,
    register_store route,
    login_store route

--- link/link/auth/views.py
+++ link/link/auth/views.py
@@ -1,6 +1,9 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user
 
 from auth import bp_auth
+from user.models import User
+from user.validation import UserValidate
 from flask_login import current_user
 
 
@@ -8,14 +11,35 @@ from flask_login import current_user
 def register_create():
     if current_user.is_authenticated():
         return redirect(url_for('index'))
-    return render_template('auth/register.html')
+    return render_template('auth/register.html', user=type(current_user))
 
 
 @bp_auth.route('/register', methods=['POST'])
 def register_store():
     if current_user.is_authenticated():
         return redirect(url_for('index'))
-    return redirect(url_for('bp_user.home'))
+    if not request.form['password'] == request.form['password_confirm']:
+        flash("Password not matched", "error")
+        return redirect(url_for('bp_auth.register_create'))
+    if request.form and 'email' in request.form.keys() and \
+            'password' in request.form.keys():
+        if UserValidate.email(request.form['email']) and \
+                UserValidate.password(request.form['password']):
+            user = User.create(
+                email=request.form['email'],
+                password_=request.form['password']
+            )
+            if user:
+                login_user(user, False)
+                flash("welcome", "success")
+                return redirect(url_for('bp_user.index'))
+            flash("Problem with server, try again", "warning")
+            return redirect(url_for('bp_auth.register_create'))
+        else:
+            flash("Someone exists with this email address", "error")
+            return redirect(url_for('bp_auth.register_create'))
+    flash("Credentials are necessary", "error")
+    return redirect(url_for('bp_auth.register_create'))
 
 
 @bp_auth.route('/login', methods=['GET'])
@@ -29,4 +53,12 @@ def login_create():
 def login_store():
     if current_user.is_authenticated():
         return redirect(url_for('index'))
-    return redirect(url_for('bp_user.home'))
+    if not request.form and 'email' in request.form.keys() and \
+            'password' in request.form.keys():
+        return redirect(url_for('bp_auth.login_create'))
+    user = User.query.filter_by(email=request.form['email'])
+    if not user and not user.verify_password(request.form['password']):
+        flash('email or password is wrong', 'error')
+        return redirect(url_for('bp_auth.login_create'))
+    login_user(user, request.form['remember'])
+    return redirect(url_for('bp_user.index'))
