44b90970335532b08b5635dd180658eeff3c8de9
majid mohamadi <m.shutdown57@gmail.com>
2018-11-26 11:49:22 +0330

    added and fixed
    fixed returning templates
    added thread execution to run task in background
    added call_back route to get image info from p2i api
    added make_img function to send request to p2i api to create screenshot
    added request_img function to handle get request to p2i api
    added write_img function to write image file and save file
    fixed shown route
    fixed search route

--- link/link/link/views.py
+++ link/link/link/views.py
@@ -1,12 +1,21 @@
-from flask import render_template
+import os
+import time
+import json
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from flask import render_template, request, flash, redirect, url_for
 from urllib.parse import urlparse
-from flask import request
+from werkzeug.utils import secure_filename
+from flask_login import current_user
 
 from common.extensions import db
-from common.utils import response
+from common.utils import allowed_file
 from link import bp_link
-from auth.auth import token_auth
+from config import Config
 from link.models import Link, Tag
+
+
+executor = ThreadPoolExecutor(max_workers=1)
 
 
 @bp_link.route('/', methods=['GET'])
@@ -15,53 +24,28 @@ def index(pages=None):
     return render_template('link/index.html', links=links)
 
 
-@bp_link.route('/pages/<int:page>', methods=['GET'])
-@bp_link.route('/', methods=['GET'])
-def get_links(page: int=None):
-    links = Link.query.paginate()
-    if not page:
-        data = {
-            'has_prev': links.has_prev,
-            'has_next': links.has_next,
-            'pages': links.pages,
-            'links': [link.to_json() for link in links.items],
-            'page': links.page,
-            'total': links.total
-        }
-        return response(data=data)
-    elif links.pages >= page:
-        links.page = page
-        data = {
-            'has_prev': links.has_prev,
-            'has_next': links.has_next,
-            'pages': links.pages,
-            'links': [link.to_json() for link in links.items],
-            'page': links.page,
-            'total': links.total
-        }
-        return response(data=data)
-    else:
-        return response(status_code=404)
+@bp_link.route('/create', methods=['GET'])
+def create():
+    return render_template('link/create.html')
 
 
-@bp_link.route('/<int:id>', methods=['GET'])
-def get_link(id: int):
-    link = Link.query.filter_by(id=id).first()
-    if link:
-        return response(data=link.to_json(), status_code=200)
-    return response(status_code=404)
-
-
-@bp_link.route('/', methods=['POST'])
-@token_auth.login_required
-def create_link():
-    data = request.get_json()
-    if data and 'url' in data and 'description' in data and 'tags' in data and data['tags']:
+@bp_link.route('/store', methods=['POST'])
+def store():
+    data = request.form.copy()
+    # TODO better validation
+    if data and 'url' in data.keys() and 'description' in data.keys() and \
+            'tags' in data.keys() and data['tags']:
         link = Link.query.filter_by(url=data['url']).first()
         if not link:
+            resp = make_img(data['url'])
+            #  img_url = get_img(resp['image_url'], data['name'])
             url_info = urlparse(data['url'])
-            link = Link(url=data['url'], description=data['description'], name=url_info.netloc)
-            for tag_name in data['tags']:
+            link = Link(
+                user_id=current_user.id,
+                url=data['url'],
+                description=data['description'],
+                name=url_info.netloc)
+            for tag_name in data['tags'].split(','):
                 old_tag = Tag.query.filter_by(name=tag_name).first()
                 if old_tag:
                     link.tags.append(old_tag)
@@ -70,70 +54,103 @@ def create_link():
                 tag = Tag(name=tag_name)
                 db.session.add(tag)
                 link.tags.append(tag)
-                db.session.add(link)
+            executor.submit(make_img, data['url'], url_info.netloc, link.id)
+            db.session.add(link)
             db.session.commit()
-            return response(data=link.to_json(), status_code=201)
-        return response(status_code=409)
-    return response(status_code=400)
+            return redirect(url_for('bp_link.show', id=link.id))
+        flash("Data exist", "error")
+        return redirect(url_for('bp_link.show', id=link.id))
+    flash("fill require inputs", "error")
+    return redirect(url_for('bp_link.create'))
 
 
-@bp_link.route('/<int:id>', methods=['PUT'])
-@token_auth.login_required
-def update_link(id: int):
+@bp_link.route('/p2i_callback', methods=['POST'])
+def call_back():
+    print("They call me!", request.form)
+    return '', 200
+
+
+def make_img(p2i_url: str, name: str, link_id: int) -> None:
+    """
+    Make image screenshut
+    :param p2i_url str: Website url to make screenshut
+    :param name str: Website name
+    :param link_id int: Link id object to store image url
+    """
+    p2i = {
+        'p2i_url': p2i_url,
+        'p2i_key': os.environ.get('P2I_KEY', ''),
+        'p2i_device': 6,
+        'p2i_fullpage': 1,
+        'p2i_imageformat': 'jpg',
+        'p2i_size': '600x0',
+        'p2i_callback': 'http://localhost:5000/links/call_back',
+        'p2i_screen': '1024x768'
+    }
+
+    request_url = 'http://api.page2images.com/restfullink'
+    data = request_img(request_url, p2i)
+    time.sleep(3000)
+    data = request_img(request_url, p2i)
+
+    # double check to sure image screenshut is done
+    if data.json()['status'] == 'processing':
+        time.sleep(3000)
+        data = request_img(request_url, p2i)
+
+    img_url = write_image(data.json()['image_url'], name)
+    if img_url:
+        link = Link.query.filter_by(id=link_id).first()
+        link.website_image = img_url
+        db.session.add(link)
+        db.session.commit()
+    #  print(data.json()['status'])
+    # "status":"processing"
+    # "status":"finished","image_url":"http://api.page2images.com/ccimages/ea/e9/0F0RnDufDEfr5pVS.jpg"
+
+
+def request_img(request_url: str, param: dict) -> dict:
+    """
+    Get request to p2i api to make screenshut
+    :param request_url str: Website we want to make screenshut
+    :param param dict: Configuration to make screenshut
+    :return: Response of p2i api
+    """
+    try:
+        data = requests.get(request_url, params=p2i)
+    except Exception as e:
+        print(f"Error in [get image url]: {e}")
+        raise # TODO enter exception class
+    return data
+
+
+def write_img(img_url, img_name):
+    """
+    Download and write image to disk
+    :param img_url str: The image url of screenshut
+    :param img_name str: Name of file
+    """
+    img_name = '-'.join(img_name.split('/')[-1])
+    #  img_url = 'http://api.page2images.com/ccimages/cf/77/tE2byaNynW4mGgQ2.jpg'
+    if not img_url:
+        print("Image url required")
+        raise # TODO enter exception class
+    try:
+        img_file = requests.get(img_url).content
+    except Exception as e:
+        print(f"Error in [get image url]: {e}")
+        raise # TODO enter exception class
+    if img_file and allowed_file(img_name):
+        img_name = secure_filename(img_name)
+        with open(Config.UPLOAD_FOLDER + '/' + img_name, 'wb') as image:
+            image.write(img_file)
+        return Config.UPLOAD_FOLDER + '/' + img_name
+
+
+@bp_link.route('/<int:id>', methods=['GET'])
+def show(id):
     link = Link.query.filter_by(id=id).first()
     if not link:
-        return response(status_code=404)
-    data = request.get_json()
-    if data:
-        for key in data:
-            if hasattr(link, key):
-                setattr(link, key, data[key])
-                db.session.add(link)
-        db.session.commit()
-        return response(data=link.to_json(), status_code=201)
-    return response(status_code=400)
-
-
-@bp_link.route('/<int:id>', methods=['DELETE'])
-@token_auth.login_required
-def delete_link(id: int):
-    link = Link.query.filter_by(id=id).first()
-    if not link:
-        return response(status_code=404)
-    url = link.url
-    db.session.delete(link)
-    db.session.commit()
-    return response(message='Link {url} has been deleted'.format(url=url),
-                    status_code=202)
-
-
-@bp_link.route('/tags', methods=['GET'])
-def get_tags():
-    tags = Tag.query.get().all()
-    if tags:
-        tags = [tag.to_json() for tag in tags]
-        return response(data=tags, status_code=200)
-    return response(status_code=404)
-
-
-@bp_link.route('/tags/<int:id>', methods=['GET'])
-@token_auth.login_required
-def update_tag(id: int):
-    tag = Tag.query.filter_by(id=id).first()
-    if not tag:
-        return response(status_code=404)
-    data = request.get_json()
-    if data and 'name' in data:
-        tag.name = data['name']
-        db.session.add(tag)
-        db.session.commit()
-        return response(data=tag.to_json(), status_code=202)
-    return response(status_code=400)
-
-
-@bp_link.route('/search', methods=['POST'])
-def search():
-    data = request.get_json()
-    if data:
-        pass
-    return response(status_code=404)
+        flash("Link does not exist", "error")
+        return redirect('bp_link.index')
+    return render_template('/link/show.html', link=link)
