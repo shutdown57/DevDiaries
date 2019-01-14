fc70c45d7f5f8d93fba56e2aaf1063025c8e0238
majid mohamadi <m.shutdown57@gmail.com>
2019-01-14 13:33:44 +0330

    fixed environ variables
    added gunicorn comnmand

--- link/link/presetup.sh
+++ link/link/presetup.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
+
+# export environment variables
+# export $(grep -v '^#' ./.env | xargs -d '\n')
+# export $(grep -v '^#' .env | xargs -d '\n')
+export $(grep -v '^#' .env | xargs)
+
+# initialize database
 python ./manage.py db init
 python ./manage.py init_db
 python ./manage.py db upgrade
-python ./manage.py set_env
+
+gunicorn wsgi -w 1 -b 0.0.0.0:5000 --reload
