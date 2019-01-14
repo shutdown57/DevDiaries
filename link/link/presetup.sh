242ecffaeaa25f5de20247f559960d4cedb807df
majid mohamadi <m.shutdown57@gmail.com>
2019-01-14 13:49:13 +0330

    cleanup

--- link/link/presetup.sh
+++ link/link/presetup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # export environment variables
-# export $(grep -v '^#' ./.env | xargs -d '\n')
-# export $(grep -v '^#' .env | xargs -d '\n')
 export $(grep -v '^#' .env | xargs)
 
 # initialize database
@@ -10,4 +8,5 @@ python ./manage.py db init
 python ./manage.py init_db
 python ./manage.py db upgrade
 
+# run application
 gunicorn wsgi -w 1 -b 0.0.0.0:5000 --reload
